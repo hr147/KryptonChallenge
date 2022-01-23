e334f0eb61fda1e74d8d1e24f4d394ce15d5f37e
HR <traderepublic@test.com>
2022-01-23 13:21:03 +0100

    configure `AppSetup` directory

---
+++ KryptonChallenge/AppSetup/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  KryptonChallenge
+//
+//  Created on 22.01.22.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ KryptonChallenge/AppSetup/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  KryptonChallenge
+//
+//  Created on 22.01.22.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
